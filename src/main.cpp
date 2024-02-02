79dccbe46e3fd9b802c132e63527c834093bdc95
mstone3201 <mbs3201@gmail.com>
2024-02-02 17:51:58 -0500

    OpenSSL
    * Added dependency to OpenSSL with vcpkg
    * Test fetching crypto object

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <openssl/evp.h>
 
 int main() {
+    EVP_CIPHER* cipher = EVP_CIPHER_fetch(nullptr, "AES-256-CBC", "provider=default");
+    EVP_CIPHER_free(cipher);
+
     std::cout << "Hello world!" << std::endl;
 }
